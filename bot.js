161c5fd1687efb281d3e1fcff97dfbfb54647a51
Gregg Thomason <gregg.thomason@asti-usa.com>
2013-12-15 21:22:48 -0500

    move sayings out and update package.json

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ bot.addListener('raw', function(message){
         bot.say('#asti', "Good morning, meatbags.");
       }
       bot.send('mode', '#asti', '+o', 'greggt');
-    })
+    });
   }
 });
 
@@ -31,11 +31,11 @@ bot.addListener('join#asti', function(nick, message){
   if (nick === 'greggt') {
     bot.send('mode', '#asti', '+o', 'greggt');
   }
-})
+});
 
 bot.addListener('error', function(message){
-  console.log(message)
-})
+  console.log(message);
+});
 
 // spawn a console if asked
 if (opts.console) {
@@ -43,38 +43,7 @@ if (opts.console) {
   r.context.bot = bot;
 }
 
-// say something dopey
-var sayings = [
-  'do you sell hubcaps for a 1972 pinto?',
-  'bring me a tangerine',
-  'Good morning, meatbags.',
-  'open GLITTER VALVE to maximum',
-  'Mi papa tiene 47 anos',
-  'butts butts butts #butts',
-  'HEADBANGER!',
-  'he dominates the DECADENT SUBWAY SCENE.',
-  'Science!',
-  'Still under development, Gary.',
-  'Pipe down, coppertop.',
-  "at what age is it okay to tell a highway that it's adopted?",
-  "you're soaking in it",
-  "My Name Is Blayze Thunderstorm and i dont care about the band Papa roach"
-];
-setInterval(function () {
-  var sayit = function(){
-    var saynum = Math.floor(Math.random() * (sayings.length - 1) + 1);
-    if (Math.floor(Math.random() * (100) + 1) <= 10) {
-      console.log("saying something " + saynum);
-      //bot.say('#asti', sayings[saynum]);
-      console.log("said " + sayings[saynum]);
-    } else {
-      console.log("not going to say anything right now.");
-    }
-  }
-  return sayit();
-}, 5000);
-
 // listen for your name
 bot.addListener('pm', function(from, message){
   console.log(from + ' => ME: ' + message);
-})
+});
