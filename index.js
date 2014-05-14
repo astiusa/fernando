d7f0aaa98ee0e51ad101ba088e10a0e21a77ac6c
Gregg Thomason <gregg.thomason@asti-usa.com>
2014-05-13 20:56:01 -0400

    first pass at restructure

---
+++ index.js
@@ -0,0 +1,27 @@
+var opts = require('commander');
+var webserver = require('./lib');
+var bot = require('./lib/bot');
+
+opts
+.option('-c, --console', 'open a console for interactivity with the bot')
+.option('-n, --name [name]', 'nickname')
+.option('-h, --host [host]', 'IRC host to connect to')
+.option('-j, --join <channels>', 'channel(s) to join on start')
+.option('-o, --op <users>', 'users to give ops when they join')
+.option('-p, --httpPort [port]', 'port for http server to'+
+        'listen on. Defaults to 4000')
+.parse(process.argv);
+
+var nick = opts.name || 'FernandoBot';
+var irchost = opts.host || 'irc.asti-usa.com';
+var join = [opts.join] || ['#asti'];
+
+var ircbot = bot.bot(irchost, nick, join);
+
+if (!opts.httpPort) {
+  opts.httpPort = 4000;
+}
+webserver.init({
+  port: opts.httpPort
+});
+webserver.start();
